74f5c339530b553f6abcf839d6378ab5c7c1b7c2
Velcis Ribeiro <velcisribeirodj@gmail.com>
2023-01-24 16:38:40 -0300

    created owner parking controller

---
+++ app/Controllers/Http/OwnerParkingsController.ts
@@ -0,0 +1,35 @@
+// import Database from '@ioc:Adonis/Lucid/Database'
+
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import OwnerParking from 'App/Models/OwnerParking'
+
+import { generateMD5 } from 'App/Utils/functions'
+import CreateOwnerParkingValidator from 'App/Validators/CreateOwnerParkingValidator'
+
+export default class OwnerParkingsController {
+  public async store({ request, response }: HttpContextContract) {
+    const data = await request.validate(CreateOwnerParkingValidator)
+
+    const passwordHashed = generateMD5(data.password)
+
+    const ownerParkingExists = await OwnerParking.findBy('ownerEmail', data.email)
+
+    if (ownerParkingExists) {
+      return response.conflict({ message: 'user with this email exists' })
+    }
+
+    // const createdParking = await Database.rawQuery("INSERT INTO parkings (ownerMame, ownerEmail, ownerPassword) VALUES (?, ?, ?)", [data.name, data.email, passwordHashed])
+
+    const createdParking = await OwnerParking.create({
+      ownerName: data.name,
+      ownerEmail: data.email,
+      ownerPassword: passwordHashed
+    })
+
+    if (createdParking) {
+      return response.created(createdParking)
+    } else {
+      return response.badRequest('something is wrong')
+    }
+  }
+}
